7dd99017376d48f0f984fe10484e51d0d0f44c25
MAunZaidi <aunzaidi118@gmail.com>
2026-08-15 17:03:28 -0700

    some-cleanup

--- backend/main.py
+++ backend/main.py
@@ -17,63 +17,33 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 @app.post("/signup", response_model=admin_schema.AdminResponse)
-async def createAdmin(
-    User:admin_schema.AdminCreate,
-    db:AsyncSession = Depends(getdb)
-):
+async def createAdmin(User:admin_schema.AdminCreate,db:AsyncSession = Depends(getdb)):
     return await admin.AdminBorn(db,User)
 
-
 @app.post("/login", response_model=admin_schema.LoginResponse)
-async def LoginAdmin(
-    LoginBody:admin_schema.LoginCreate,
-    db:AsyncSession = Depends(getdb)
-):
+async def LoginAdmin(LoginBody:admin_schema.LoginCreate, db:AsyncSession = Depends(getdb)):
     return await admin.LoginLogic(db, LoginBody)
 
-
 @app.get("/is_auth", response_model=admin_schema.AdminResponse)
-async def is_auth(
-    request: Request,
-    db:AsyncSession = Depends(getdb)
-):
+async def is_auth(request: Request, db:AsyncSession = Depends(getdb)):
     return await helper.is_auth(request, db)
 
-
 @app.post("/members", response_model=member_schema.MemberData)
-async def CreateMember(
-    MemberBody: member_schema.MemberData,
-    db:AsyncSession = Depends(getdb)  
-):
+async def CreateMember(MemberBody: member_schema.MemberData, db:AsyncSession = Depends(getdb)):
     return await member.RegisterMember(db,MemberBody)
 
-
 @app.get("/members", response_model=list[member_schema.MemberData])
-async def GetMembers(
-    db:AsyncSession = Depends(getdb)
-):
+async def GetMembers(db:AsyncSession = Depends(getdb)):
     return await member.GetMember(db)
 
-
 @app.get("/members/{memberid}", response_model=member_schema.MemberData)
-async def GetMembers(
-    memberid:int,
-    db:AsyncSession = Depends(getdb)
-):
+async def GetMembers(memberid:int, db:AsyncSession = Depends(getdb)):
     return await member.GetMemberByid(db, memberid)
 
 @app.put("/members/{memberid}", response_model=member_schema.MemberData)
-async def UpdateMember(
-    memberid:int,
-    MemberBody:member_schema.MemberData,
-    db:AsyncSession = Depends(getdb)
-):
+async def UpdateMember(memberid:int, MemberBody:member_schema.MemberData, db:AsyncSession = Depends(getdb)):
     return await member.UpdateMember(db, memberid, MemberBody)
 
-
 @app.delete("/members/{memberid}")
-async def DeleteMember(
-    memberid:int,
-    db:AsyncSession = Depends(getdb)
-):
+async def DeleteMember(memberid:int, db:AsyncSession = Depends(getdb)):
     return await member.DeleteMember(db, memberid)
